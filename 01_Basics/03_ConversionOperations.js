0fd63355246c564ebd8923d281eb3f74f300183b
praveenkr-07 <praveenmahour098@outlook.com>
2026-05-25 00:59:25 +0530

    Add conversion operations example in ConversionOperations.js

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,15 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" =>  NaN
+// true = 1 , false = 0
+//  1 -> Boolean(1) => true 
+// 0 -> Boolean(0) => false
+// "" -> Boolean("") => false
