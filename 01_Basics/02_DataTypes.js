86a192518a265ac27ca8985442384cea7e1a6fc8
praveenkr-07 <praveenmahour098@outlook.com>
2026-05-24 19:10:20 +0000

    Add datatypes tutorial file

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,11 @@
+// number
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined
+// symbol => unique
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
