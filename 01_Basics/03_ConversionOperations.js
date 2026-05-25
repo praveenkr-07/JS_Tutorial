edab395bc5fa3dbc7299c1fd41b0c5f1e55a9618
praveenkr-07 <praveenmahour098@outlook.com>
2026-05-26 00:56:07 +0530

    Update 03_ConversionOperations.js

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -13,3 +13,23 @@ console.log(valueInNumber);
 //  1 -> Boolean(1) => true 
 // 0 -> Boolean(0) => false
 // "" -> Boolean("") => false
+
+// *******************Operations******************
+
+// console.log(2+3);
+// console.log(4-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(4/3);
+// console.log(4%3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+
+// console.log("1"+2+2 ); // 122
+// console.log(typeof ("1"+2+2) ); // stirng
+
+// console.log(1+2+"2" ); // 32
+// console.log(typeof (1+2+"2") ); // string
+
+// console.log(1+"2"+2 ); // 122 // string
